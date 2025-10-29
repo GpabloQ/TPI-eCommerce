12fc7823e78511ed36ee4bf578cbff13e954b749
FRANCISCO NAHUEL FONSECA <francisco60036@hotmail.com>
2025-10-28 22:55:17 -0300

    Se agrego la pagina Registrarse

---
+++ Capa Solucion/WebAppEcommerce/Registrarse.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppEcommerce
+{
+    public partial class Registrarse : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtDni_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+    }
+}
