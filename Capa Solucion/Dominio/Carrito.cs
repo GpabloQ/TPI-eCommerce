c70b0a40a14d3be30a4592b4407530c3dc10d385
FRANCISCO NAHUEL FONSECA <francisco60036@hotmail.com>
2025-11-20 23:54:09 -0300

    Agregue las clases carrito y elementoCarrito

---
+++ Capa Solucion/Dominio/Carrito.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Carrito
+    {
+        public long IdCarrito { get; set; }
+        public long IdUsuario { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string Estado { get; set; }
+
+        public List<ElementoCarrito> Items { get; set; }
+    }
+}
---
+++ Capa Solucion/Dominio/ElementoCarrito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ElementoCarrito
+    {
+        public long IdElemento { get; set; }
+        public long IdCarrito { get; set; }
+        public long IdArticulo { get; set; }
+        public long Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+}
