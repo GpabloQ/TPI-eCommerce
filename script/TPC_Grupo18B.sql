9e7295110d29da6ef406cae952d38f7833ad3b05
Francisco Nahuel Fonseca <128767970+fran082@users.noreply.github.com>
2025-11-26 19:59:50 -0300

    Se movieron algunas tablas de lugar

--- script/TPC_Grupo18B.sql
+++ script/TPC_Grupo18B.sql
@@ -24,6 +24,19 @@ create table TIPOUSUARIOS(
 );
 
 go
+create table DOMICILIOS (
+	IdDomicilio bigint not null primary key identity (1,1),
+	Calle varchar(100) not null,
+	Ciudad varchar(100) not null,
+	Departamento varchar (100) null,
+	Numero bigint not null,
+	Piso bigint null, 
+	Provincia varchar(100) not null,
+	CodigoPostal bigint not null,
+	Estado bit null
+);
+
+go
 create table USUARIOS (
 	IdUsuario bigint not null primary key identity (1,1),
 	Nombre varchar(100) not null,
@@ -59,18 +72,7 @@ create table IMAGENES (
 	UrlImagen varchar (250) null
 );
 
-go
-create table DOMICILIOS (
-	IdDomicilio bigint not null primary key identity (1,1),
-	Calle varchar(100) not null,
-	Ciudad varchar(100) not null,
-	Departamento varchar (100) null,
-	Numero bigint not null,
-	Piso bigint null, 
-	Provincia varchar(100) not null,
-	CodigoPostal bigint not null,
-	Estado bit null
-);
+
 
 go
 create table CARRITO(
@@ -90,5 +92,3 @@ create table ELEMENTOCARRITO (
 	Cantidad bigint,
 	PrecioUnitario decimal(18,2)
 );
-
-
